a6bc526512525cc3056b36e2231e5eb753331860
petkevich39 <petkevich.kirill@icloud.com>
2020-11-05 21:20:12 +0200

    added logic of calc

--- calc.php
+++ calc.php
@@ -1,13 +1,42 @@
 <?php
-function Calc(){
-	return fgets(STDIN);
-}
-$input = Calc();
-do
+function Init()
 {
-	$input = $input + Calc();
-	echo $input;
+	echo 'Введите число:';
+	$input = GetNumb();
+	Calc($input);
 }
-while ($input != 0);
-
-
+function Calc($input)
+{
+	do
+	{
+		echo 'Введите ещё одно число:';
+		$numb = GetNumb();
+		$input = $input + $numb;
+		echo 'Сумма чисел равна: '.$input, PHP_EOL;
+		echo 'Желаете продолжить?(Y/n)', PHP_EOL;
+		$contin = trim(fgets(STDIN));
+		if ($contin != 'Y')
+		{
+			echo 'Желаете начать заново?(Y/n)', PHP_EOL;
+			$contin = trim(fgets(STDIN));
+			if ($contin != 'Y')
+			{
+				break;
+			} else {
+				Init();
+			}
+		}
+	}
+	while ($input != 0);
+}
+function GetNumb(){
+	$numb = trim(fgets(STDIN));
+	if(!empty($numb) && is_numeric($numb)){
+		return $numb;
+	} else {
+		echo 'Это было не число! >.<',PHP_EOL;
+		echo 'Попробуйте снова!',PHP_EOL;
+		return GetNumb();
+	}
+}
+Init();
