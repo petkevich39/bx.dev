5ee87013a873152811e2d645c6289b39c1ca9f2a
petkevich39 <petkevich.kirill@icloud.com>
2020-11-18 17:38:42 +0200

    fix tests.php and read.php

--- read.php
+++ read.php
@@ -1,11 +1,11 @@
 <?php
 
-function readFromConsole($msg, $value)
+function readFromConsole($message, $value)
 {
-	if(empty($msg) && empty($value))
+	if(empty($message) && empty($value))
 	{
 		echo 'Введите сообщение:'.PHP_EOL;
-		$msg = trim(fgets(STDIN));
+		$message = trim(fgets(STDIN));
 		echo 'Введите значение:'.PHP_EOL;
 		$value = trim(fgets(STDIN));
 	}
@@ -13,26 +13,26 @@ function readFromConsole($msg, $value)
 	switch ($value)
 	{
 		case is_numeric($value):
-			echo $msg . 'Результат: '.$value.PHP_EOL;
+			echo $message . 'Результат: '.$value.PHP_EOL;
 			return $value + 0;
 		case is_string($value):
 			if($value == '!stop')
 			{
-				echo $msg . 'Результат: null'.PHP_EOL;
+				echo $message . 'Результат: null'.PHP_EOL;
 				return null;
 			}
 			elseif($value == 'false')
 			{
-				echo $msg . 'Результат: false'.PHP_EOL;
+				echo $message . 'Результат: false'.PHP_EOL;
 				return false;
 			}
 			elseif($value == 'true')
 			{
-				echo $msg . 'Результат: true'.PHP_EOL;
+				echo $message . 'Результат: true'.PHP_EOL;
 				return true;
 			}
 			else {
-				echo $msg . 'Результат: '.$value.PHP_EOL;
+				echo $message . 'Результат: '.$value.PHP_EOL;
 				return (string)$value;
 			}
 		default:
--- tests.php
+++ tests.php
@@ -1,13 +1,13 @@
 <?php
 require_once ('read.php');
 
-function assertEquals($dreamval, $result, $msg){
+function assertEquals($dreamval, $result, $message){
 	if($dreamval === $result){
-		echo $msg . ' - passed';
+		echo $message . ' - passed'.PHP_EOL.PHP_EOL;
 	}
 	else
 	{
-		echo $msg . ' - failed';
+		echo $message . ' - failed'.PHP_EOL.PHP_EOL;
 	}
 
 }
@@ -16,6 +16,21 @@ function assertEquals($dreamval, $result, $msg){
 
 $result = readFromConsole('', '!stop');
 assertEquals(null, $result, 'Test: !stop = null');
+
+$result = readFromConsole('', 'true');
+assertEquals(true, $result, 'Test: true = true');
+
+$result = readFromConsole('', 'false');
+assertEquals(false, $result, 'Test: false = false');
+
+$result = readFromConsole('', '1.3');
+assertEquals(1.3, $result, 'Test: 1.3 = 1.3');
+
+$result = readFromConsole('', '1');
+assertEquals(1, $result, 'Test: 1 = 1');
+
+$result = readFromConsole('', 'test');
+assertEquals('test', $result, 'Test: test = test');
 
 // * readFromConsole('', 'true') - true;
 // * readFromConsole('', 'false') - false;
