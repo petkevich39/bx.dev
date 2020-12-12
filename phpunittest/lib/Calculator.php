e6fdd166bd45d1aa1c201007ae98755c436f79ef
petkevich39 <petkevich.kirill@icloud.com>
2020-12-12 11:37:29 +0200

    fixed calc test

--- phpunittest/lib/Calculator.php
+++ phpunittest/lib/Calculator.php
@@ -24,17 +24,20 @@ class Calculator
 		{
 			throw new InvalidArgumentException('Divider cant be a zero');
 		}
-
 		return $a / $b;
 	}
 
-	public function elevate(int $a, int $b): int
+	public function elevate(int $a, int $b): float
 	{
 		return pow($a, $b);
 	}
 
-	public function squareRoot(int $a): int
+	public function squareRoot(int $a): float
 	{
+		if($a < 0)
+		{
+			throw new InvalidArgumentException('The number cannot be less than zero');
+		}
 		return sqrt($a);
 	}
 }
--- phpunittest/tests/CalculatorTest.php
+++ phpunittest/tests/CalculatorTest.php
@@ -9,19 +9,62 @@ class CalculatorTest extends TestCase
 	{
 		$calculator = new Calculator();
 		self::assertEquals(4, $calculator->add(2, 2));
-
-		self::assertEquals(10, $calculator->subtract(12, 2));
-
-		self::assertEquals(30, $calculator->multiply(6, 5));
-
-		self::assertEquals(8, $calculator->elevate(2, 3));
-
-		self::assertEquals(10, $calculator->squareRoot( 100));
-
-		self::assertEquals(1, $calculator->divide(2, 2));
-
-		self::assertEquals(0, $calculator->divide(2, 0));
-
+		self::assertEquals(5, $calculator->add(5, 0));
+		self::assertEquals(-2, $calculator->add(-1, -1));
 	}
 
+	public function testSubtract(): void
+	{
+		$calculator = new Calculator();
+		self::assertEquals(10, $calculator->subtract(12, 2));
+		self::assertEquals(0, $calculator->subtract(12, 12));
+		self::assertEquals(-1, $calculator->subtract(12, 13));
+	}
+
+	public function testMultiply(): void
+	{
+		$calculator = new Calculator();
+		self::assertEquals(30, $calculator->multiply(6, 5));
+		self::assertEquals(-2, $calculator->multiply(1, -2));
+		self::assertEquals(0, $calculator->multiply(1, 0));
+	}
+
+	public function testDivide(): void
+	{
+		$calculator = new Calculator();
+		self::assertEquals(1, $calculator->divide(2, 2));
+		self::assertEquals(0.5, $calculator->divide(1, 2));
+	}
+
+	public function testElevate(): void
+	{
+		$calculator = new Calculator();
+		self::assertEquals(8, $calculator->elevate(2, 3));
+		self::assertEquals(0.1, $calculator->elevate(10, -1));
+		self::assertEquals(1, $calculator->elevate(2, 0));
+	}
+
+	public function testSquareRoot(): void
+	{
+		$calculator = new Calculator();
+		self::assertEquals(2.2360679774998, $calculator->squareRoot( 5));
+		self::assertEquals(10, $calculator->squareRoot( 100));
+		self::assertEquals(0, $calculator->squareRoot( 0));
+	}
+
+	public function testExceptionDivide(): void
+	{
+		$calculator = new Calculator();
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Divider cant be a zero');
+		$calculator->divide(5, 0);
+	}
+
+	public function testExceptionSquareRoot(): void
+	{
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('The number cannot be less than zero');
+		$calculator = new Calculator();
+		$calculator->squareRoot(-25);
+	}
 }
