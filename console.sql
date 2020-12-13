77a727e97789a5d7db632d6c20abbbd9a46beeac
petkevich39 <petkevich.kirill@icloud.com>
2020-12-13 20:10:57 +0200

    Added first Tusk for databases

---
+++ console.sql
@@ -0,0 +1,48 @@
+create table store
+(
+	ID int not null auto_increment,
+	CITY varchar(500) not null,
+	primary key(ID)
+);
+
+show tables from dev;
+
+insert into store (ID, CITY)
+values (1, '???????????'),
+       (2, '??????????'),
+       (3, '???????');
+
+create table book_store
+(
+	BOOK_ID int not null,
+	STORE_ID int not null,
+	PRICE decimal (10, 2),
+	QUANTITY int not null default 0,
+	primary key (BOOK_ID, STORE_ID),
+	foreign key FK_BOOK_STORE_STORE (STORE_ID) references store(ID)
+		on update restrict
+		on delete restrict,
+	foreign key FK_BOOK_STORE_BOOK (BOOK_ID) references book(ID)
+		on update restrict
+		on delete restrict
+);
+
+show tables from dev;
+
+insert into book_store (BOOK_ID, STORE_ID, PRICE, QUANTITY)
+select ID, 1, PRICE, QUANTITY
+from book;
+
+insert into book_store (BOOK_ID, STORE_ID, PRICE, QUANTITY)
+select ID, 2, PRICE + 20, QUANTITY
+from book;
+
+insert into book_store (BOOK_ID, STORE_ID, PRICE, QUANTITY)
+select ID, 3, PRICE + 17.50, QUANTITY + 5
+from book;
+
+alter table book
+	drop column PRICE;
+
+alter table book
+	drop column QUANTITY;
